d64e395fe6381e32b5ff62f1b8cf2de51b03f814
jbbrissaud <jb.brissaud@gmail.com>
2022-09-10 21:59:02 +0200

    ok

---
+++ backend/src/main/scala/main.scala
@@ -0,0 +1,41 @@
+package example
+
+import zhttp.http._
+import zhttp.service.Server
+import zio.*
+import zio.json.*
+
+import java.io.File
+
+object WebServer extends ZIOAppDefault:
+
+  def getFile(filename:String) =
+    val file = new File(filename)
+    Http.fromFile(file)
+  
+  private val appBase = 
+    Http.collectHttp[Request] {
+      case Method.GET -> !! =>
+        val filename = "frontend/static/index.html"
+        getFile(filename)
+      case Method.GET -> !! / "favicon.ico"=>
+        val filename = "frontend/static/plante.ico"
+        getFile(filename)
+      case Method.GET -> !! / "static" / "main.js" =>
+        val filename = "frontend/target/scala-3.1.1/frontend-fastopt/main.js"
+        getFile(filename)
+      case Method.GET -> !! / "static" / name =>
+        val filename = s"frontend/static/$name"
+        getFile(filename)
+    }
+  private val appHello: Http[Any, Nothing, Request, Response] =
+    Http.collectZIO[Request] {
+      case Method.GET -> !! / "hello" / name  => 
+        //ZIO.succeed(Response.text(s"Hello $name!"))
+        ZIO.succeed(Response.text(s"Bonjour ${name}!"))
+    }
+
+  private val app = appHello ++ appBase
+  def run = Server.start(8090, app)
+
+end WebServer
---
+++ frontend/src/main/scala/App.scala
@@ -0,0 +1,28 @@
+package example
+
+import com.raquo.laminar.api.L._
+import org.scalajs.dom
+
+import zio.*
+
+val myRuntime = Runtime.default.unsafe
+
+implicit class RunSyntax[E,A](io: ZIO[Any, E, A]) {
+  def unsafeRun: A =
+    Unsafe.unsafeCompat { implicit u =>
+      myRuntime.run(io).getOrThrowFiberFailure()
+    }
+}
+
+object App:
+
+  def appComponent = 
+    //div("Salut")
+    val myZio = ZIO.succeed(div("salut"))
+    myZio.unsafeRun
+  def main(args: Array[String]): Unit =
+    val _ = documentEvents.onDomContentLoaded.foreach { _ =>
+      val appContainer = dom.document.querySelector("#app")
+      appContainer.innerHTML = ""
+      val _            = render(appContainer, appComponent)
+    }(unsafeWindowOwner)
